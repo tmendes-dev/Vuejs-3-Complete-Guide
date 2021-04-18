567aae53e42cf29dbe2291d366db15aef9720bdd
Thomas Mendes <th.mendes1997@gmail.com>
2021-04-18 18:50:43 -0300

    Computed Dynamic Class

--- Prática 4/app.js
+++ Prática 4/app.js
@@ -6,6 +6,17 @@ const app = Vue.createApp({
       boxSelected3: false,
     };
   },
+  computed: {
+    box1Classes() {
+      return { active: this.boxSelected1 };
+    },
+    box2Classes() {
+      return { active: this.boxSelected2 };
+    },
+    box3Classes() {
+      return { active: this.boxSelected3 };
+    },
+  },
   methods: {
     boxSelected(box) {
       if (box == 1) {
